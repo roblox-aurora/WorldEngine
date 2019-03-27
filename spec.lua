20a56a75b5694dab3ef06a2706adbd0641a9cba8
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-03-28 10:56:55 +1300

    Add server code

--- spec.lua
+++ spec.lua
@@ -1,11 +1,11 @@
 --[[
 	Loads our library and all of its dependencies, then runs tests using TestEZ.
 ]]
-
 -- If you add any dependencies, add them to this table so they'll be loaded!
 local LOAD_MODULES = {
-	{"lib/shared", "WorldEngineLib"},
-	{"modules/testez/lib", "TestEZ"},
+	{"lib/shared", "WorldEngineLib", "ReplicatedStorage"},
+	{"lib/server", "WorldEngineServer"},
+	{"modules/testez/lib", "TestEZ"}
 }
 
 -- This makes sure we can load Lemur and other libraries that depend on init.lua
@@ -16,6 +16,8 @@ local lemur = require("modules.lemur")
 
 -- Create a virtual Roblox tree
 local habitat = lemur.Habitat.new()
+local game = habitat.game
+local replicated = game:GetService("ReplicatedStorage")
 
 -- We'll put all of our library code and dependencies here
 local Root = lemur.Instance.new("Folder")
@@ -25,13 +27,19 @@ Root.Name = "Root"
 for _, module in ipairs(LOAD_MODULES) do
 	local container = habitat:loadFromFs(module[1])
 	container.Name = module[2]
-	container.Parent = Root
+
+	local parent = module[3]
+	if (parent) then
+		container.Parent = game:GetService(parent)
+	else
+		container.Parent = Root
+	end
 end
 
 -- Load TestEZ and run our tests
 local TestEZ = habitat:require(Root.TestEZ)
 
-local results = TestEZ.TestBootstrap:run({Root.WorldEngineLib}, TestEZ.Reporters.TextReporter)
+local results = TestEZ.TestBootstrap:run({replicated.WorldEngineLib, Root.WorldEngineServer}, TestEZ.Reporters.TextReporter)
 
 -- Did something go wrong?
 if results.failureCount > 0 then
