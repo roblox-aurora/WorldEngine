7200a67dc2e96bf0bddb4e15baa4eeeaac0fe71f
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-03-28 14:23:01 +1300

    Added more quest testing

--- lib/server/Quests/Quest.spec.lua
+++ lib/server/Quests/Quest.spec.lua
@@ -8,8 +8,40 @@ return function()
 			expect(testQuest).to.be.ok()
 			expect(testQuest.Id).to.equal("quest-name")
 			expect(testQuest.Name).to.equal("Quest Name")
+		end
+	)
 
-			local testQuest2 = Quest.new("quest-name", "Quest Name 2");
+	it(
+		"should not allow id duplicates",
+		function()
+			expect(
+				function()
+					Quest.new("quest-name", "Quest Name 2")
+				end
+			).to.throw()
+		end
+	)
+
+	it(
+		"should not allow invalid ids",
+		function()
+			expect(
+				function()
+					Quest.new("cool quest", "Quest Name 3")
+				end
+			).to.throw()
+
+			expect(
+				function()
+					Quest.new("c%ool", "Quest Name 3")
+				end
+			).to.throw()
+
+			expect(
+				function()
+					Quest.new("coolQuest", "Quest Name 3")
+				end
+			).to.throw()
 		end
 	)
 end
