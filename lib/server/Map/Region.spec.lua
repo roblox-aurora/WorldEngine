f946632c30d2adeb45be1d4bde14b181264d7f8f
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-04-06 14:16:53 +1300

    Fixed whitespace

--- lib/server/Map/Region.spec.lua
+++ lib/server/Map/Region.spec.lua
@@ -1,7 +1,7 @@
 local Region = require(script.Parent.Region)
 
 return function()
-	it("should be able to create Regions", function() 
+	it("should be able to create Regions", function()
 		local region = Region.new(Vector3.new(0, 0, 0), Vector3.new(1, 1, 1), nil, "TestRegion")
 		expect(region).to.be.ok()
 		expect(region:GetPoint0()).to.equal(Vector3.new(0, 0, 0))
