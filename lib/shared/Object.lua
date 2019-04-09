c469d39387979bfd817f0bd072da4507c1fa70e2
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-04-09 12:46:04 +1200

    Moved shared object functions

--- lib/shared/Object.lua
+++ lib/shared/Object.lua
@@ -33,6 +33,54 @@ function Object.Enum(_, name, values)
 	return enum
 end
 
+-- Used to check, like Rbx:IsA
+local function class_IsType(object, className)
+	local meta = getmetatable(object)
+	if (meta._instance) then
+		for _, baseClass in next, meta._class._inheritance do
+			if (baseClass._className == className) then
+				return true
+			end
+		end
+
+		return false
+	else
+		error("Cannot call IsA on a class type", 2)
+	end
+end
+
+local function class_Is(self, value)
+	local selfmeta = getmetatable(self)
+	local meta = type(value) == "table" and getmetatable(value)
+
+	if not selfmeta or selfmeta._instance then
+		error("`Is` must be called on a class!", 2)
+	end
+
+	if meta and meta._instance then
+		return value:IsType(self._className)
+	else
+		return false
+	end
+end
+
+-- Used to pass inheritance
+local function class_super(_class, obj, ...)
+	-- luacov: disable
+	if (type(obj) ~= "table") then
+		error("First argument of super must be an object!", 2)
+	end
+
+	if (_class == getmetatable(obj).__index) then
+		error("Call 'super' on the base class rather than the same class!", 2)
+	end
+	-- luacov: enable
+
+	if type(_class.constructor) == "function" then
+		_class.constructor(obj, ...)
+	end
+end
+
 --- Class:Extend(str) for class extending, default is Object:Extend( ) for base classes
 function Object:Extend(name, options)
 	assert(type(name) == "string")
@@ -70,53 +118,9 @@ function Object:Extend(name, options)
 
 	setmetatable(class, super)
 
-	-- Used to check, like Rbx:IsA
-	function class.IsType(object, className)
-		local meta = getmetatable(object)
-		if (meta._instance) then
-			for _, baseClass in next, meta._class._inheritance do
-				if (baseClass._className == className) then
-					return true
-				end
-			end
-
-			return false
-		else
-			error("Cannot call IsA on a class type", 2)
-		end
-	end
-
-	function class.Is(_self, value)
-		local selfmeta = getmetatable(_self)
-		local meta = type(value) == "table" and getmetatable(value)
-
-		if not selfmeta or selfmeta._instance then
-			error("`Is` must be called on a class!", 2)
-		end
-
-		if meta and meta._instance then
-			return value:IsType(name)
-		else
-			return false
-		end
-	end
-
-	-- Used to pass inheritance
-	function class.super(_class, obj, ...)
-		-- luacov: disable
-		if (type(obj) ~= "table") then
-			error("First argument of super must be an object!", 2)
-		end
-
-		if (_class == getmetatable(obj).__index) then
-			error("Call 'super' on the base class rather than the same class!", 2)
-		end
-		-- luacov: enable
-
-		if type(_class.constructor) == "function" then
-			_class.constructor(obj, ...)
-		end
-	end
+	class.Is = class_Is
+	class.IsType = class_IsType
+	class.super = class_super
 
 	-- Handler for X.new(...) - Calls X.constructor(...) internally
 	if not abstract then
