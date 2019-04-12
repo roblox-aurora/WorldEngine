f7ad7aef314cb3d47d78cf63a161d8434c5b8960
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-04-12 19:36:03 +1200

    Added table extension library

---
+++ lib/shared/table.lua
@@ -0,0 +1,116 @@
+local table_remove = table.remove
+local table_concat = table.concat
+local table = setmetatable({}, {__index = table})
+
+function table.map(t, mapFn)
+	assert(type(t) == "table", "Argument #1 to table.map - expected table got " .. type(t))
+	assert(type(mapFn) == "function", "Argument #2 to table.map - expected function got " .. type(mapFn))
+
+	local mapped = {}
+
+	for i = 1, #t do
+		local nextValue = mapFn(t[i], i)
+		table.insert(mapped, nextValue)
+	end
+
+	return mapped
+end
+
+function table.filter(t, filterFn)
+	assert(type(t) == "table", "Argument #1 to table.filter - expected table got " .. type(t))
+	assert(type(filterFn) == "function", "Argument #2 to table.filter - expected function got " .. type(filterFn))
+
+	local filtered = {}
+	for i = 1, #t do
+		local value = t[i]
+		if filterFn(value) then
+			table.insert(filtered, value)
+		end
+	end
+
+	return filtered
+end
+
+function table.find(t, findFn)
+	assert(type(t) == "table", "Argument #1 to table.find - expected table got " .. type(t))
+	assert(type(findFn) == "function", "Argument #2 to table.find - expected function got " .. type(findFn))
+
+	for i = 1, #t do
+		local value = t[i]
+		if findFn(value, i) then
+			return value, i
+		end
+	end
+end
+
+local indent = 0
+local function table_to_str(t, prettyPrint)
+	local values = {}
+	local char = type(prettyPrint) == "string" and prettyPrint or "\t"
+	for k, value in next, t do
+		local isNumericKey = type(k) == "number"
+
+		if type(value) == "table" then
+			indent = indent + 1
+			value = table_to_str(value, prettyPrint)
+			indent = indent - 1
+		elseif type(value) == "function" then
+			value = "<function()>"
+		elseif type(value) == "userdata" then
+			value = "<userdata: " .. tostring(value) .. ">"
+		end
+
+		table.insert(
+			values,
+			(prettyPrint and "%s" .. char .. "%s = %s" or "%s%s = %s"):format(
+				prettyPrint and (char):rep(indent) or "",
+				isNumericKey and "[" .. tostring(k) .. "]" or k,
+				value
+			)
+		)
+	end
+
+	return "{" ..
+		(prettyPrint and "\n" or "") ..
+			table_concat(values, prettyPrint and ",\n" or ", ") .. (prettyPrint and "\n" .. (char):rep(indent) or "") .. "}"
+end
+
+function table.tostring(t, pretty)
+	return table_to_str(t, pretty)
+end
+
+-- Improved table.concat
+function table.concat(t, sep)
+	local values = {}
+	for i = 1, #t do
+		local value = t[i]
+		if type(value) == "table" then
+			table.insert(values, "[ " .. table.concat(value, sep) .. " ]")
+		else
+			table.insert(values, tostring(value))
+		end
+	end
+
+	return table_concat(values, sep)
+end
+
+-- Improved table.remove
+function table.remove(t, query)
+	if type(query) == "number" then
+		return table_remove(t, query)
+	elseif type(query) == "function" then
+		for index, value in next, t do
+			if (query(value, index)) then
+				table_remove(t, index)
+			end
+		end
+	else
+		for index, value in next, t do
+			if (value == query) then
+				return table_remove(t, index)
+			end
+		end
+	end
+end
+
+return table
---
+++ lib/shared/table.spec.lua
@@ -0,0 +1,86 @@
+local table = require(script.Parent.table)
+
+return function()
+	it(
+		"should handle mapping",
+		function()
+			local mapped =
+				table.map(
+				{"a", "b", "c", "d"},
+				function(value)
+					return value:upper()
+				end
+			)
+			expect(mapped[1]).to.equal("A")
+			expect(mapped[2]).to.equal("B")
+			expect(mapped[3]).to.equal("C")
+			expect(mapped[4]).to.equal("D")
+		end
+	)
+
+	it(
+		"should handle finding",
+		function()
+			local found =
+				table.find(
+				{"hello", "world"},
+				function(value)
+					return value == "hello"
+				end
+			)
+			expect(found).to.equal("hello")
+		end
+	)
+
+	it(
+		"should handle filter",
+		function()
+			local filter =
+				table.filter(
+				{1, 2, 3, 4, 5, 6},
+				function(value)
+					return value % 2 == 0
+				end
+			)
+			for i = 1, #filter do
+				expect(filter[i] % 2).to.equal(0)
+			end
+		end
+	)
+
+	it(
+		"should handle variations of table.remove",
+		function()
+			local testTable = {1, 2, 3, 4, "John Doe"}
+			table.remove(testTable, "John Doe")
+
+			expect(
+				table.find(
+					testTable,
+					function(value)
+						return value == "John Doe"
+					end
+				)
+			).never.to.be.ok()
+
+			table.remove(
+				testTable,
+				function(value)
+					return value % 2 == 0
+				end
+			)
+
+			expect(testTable[2]).to.equal(3)
+		end
+	)
+
+	it(
+		"should handle table.tostring",
+		function()
+			expect(table.tostring({1, 2, 3, 4})).to.equal("{[1] = 1, [2] = 2, [3] = 3, [4] = 4}")
+			expect(table.tostring({a = 1, b = 2})).to.equal("{a = 1, b = 2}")
+			expect(table.tostring({1, 2, {3, 4}})).to.equal("{[1] = 1, [2] = 2, [3] = {[1] = 3, [2] = 4}}")
+			expect(table.tostring({a = 1, b = { c = 2 }})).to.equal("{a = 1, b = {c = 2}}")
+		end
+	)
+end
